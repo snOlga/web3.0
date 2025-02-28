60da1882d48fc75adb67a13de18968417081cbc7
snOlga <olga.safonova@outlook.com>
2025-02-28 16:01:47 +0300

    web socket

--- ozon/src/main/java/web/ozon/service/CommentRequestService.java
+++ ozon/src/main/java/web/ozon/service/CommentRequestService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import web.ozon.DTO.CommentDTO;
 import web.ozon.entity.CommentEntity;
@@ -18,11 +19,14 @@ public class CommentRequestService {
     private CommentRepository commentRepository;
     @Autowired
     private CommentRequestRepository commentRequestRepository;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
     @Transactional(isolation = Isolation.READ_UNCOMMITTED)
     public void createRequest(CommentDTO commentDTO) {
         CommentEntity commentEntity = commentRepository.findById(commentDTO.getId()).get();
         CommentRequestEntity commentRequestEntity = new CommentRequestEntity(null, commentEntity, false);
         commentRequestRepository.save(commentRequestEntity);
+        messagingTemplate.convertAndSend("/topic/comment-request/" + commentRequestEntity.getId());
     }
 }
